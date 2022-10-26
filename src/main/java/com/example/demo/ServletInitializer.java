366de30af65bd05a94ed832fd9cd419470884720
chiluvuri prudviraj <prudvirajchiluvuri@gmail.com>
2022-10-26 18:09:03 +0530

    modifed env..& server intilizer

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -7,6 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("ServletInitializer.configure()");
 		return application.sources(GitHubApplication.class);
 	}
 
